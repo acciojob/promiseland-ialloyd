0cd1400656a8c9a99259deb8e591b12326dfc620
ialloyd <135848926+ialloyd@users.noreply.github.com>
2023-10-06 11:51:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,67 @@
-// complete the js code
 class CustomPromise {
-  // write your code here
+    constructor(executor) {
+        this.promiseChain = [];
+        this.handleError = () => {};
+
+        this.onResolve = this.onResolve.bind(this);
+        this.onReject = this.onReject.bind(this);
+
+        try {
+            executor(this.onResolve, this.onReject);
+        } catch (error) {
+            this.promiseChain = [];
+            this.onReject(error);
+        }
+    }
+
+    then(onResolve) {
+        this.promiseChain.push(onResolve);
+
+        return this;
+    }
+
+    catch(handleError) {
+        this.handleError = handleError;
+
+        return this;
+    }
+
+    finally(onFinally) {
+        const noop = () => {};
+        const onFinallyRejectionHandler = () => onFinally().then(() => { throw reason });
+        
+        return this.then(onFinally, onFinallyRejectionHandler);
+    }
+
+    onResolve(value) {
+        let storedValue = value;
+
+        try {
+            this.promiseChain.forEach((nextFunction) => {
+                storedValue = nextFunction(storedValue);
+            });
+        } catch (error) {
+            this.promiseChain = [];
+
+            this.onReject(error);
+        }
+    }
+
+    onReject(error) {
+        this.handleError(error);
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
 
 window.CustomPromise = CustomPromise;
